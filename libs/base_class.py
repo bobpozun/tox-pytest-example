a637b0335abc1fc64cfef1e8bdc432640c32aba1
bobpozun <robert.pozun@gmail.com>
2020-10-07 21:29:00 -0400

    Update base_class.py

--- libs/base_class.py
+++ libs/base_class.py
@@ -6,13 +6,16 @@ import datetime
 class BaseClass:
     import logging
     LOG_LEVEL = logging.INFO
+    log = logging.getLogger('logger')
+    log.setLevel(LOG_LEVEL)
     date = datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d-%Y')
     directory = 'logs/%s' % date
     if not os.path.exists(directory):
         os.makedirs(directory)
     logfile = '%s/%s.log' % (directory, datetime.datetime.fromtimestamp(time.time()).strftime('%H-%M-%S'))
-    logging.basicConfig(filename=logfile, level=LOG_LEVEL)
-    log = logging.getLogger('logger')
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(LOG_LEVEL)
+    log.addHandler(fh)
 
     @classmethod
     def log_this(cls, message, is_fatal=False):
